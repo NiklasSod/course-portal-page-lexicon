44abd7e85427a3c3ccace4a1f198ca2b715405dc
Anton <antonroback@gmail.com>
2026-08-04 11:04:39 +0200

    Created Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import styles from './Footer.module.css'
+
+function Footer (){
+
+    return(
+        <footer>
+            <p>Kursportalen © 2026</p>
+            <nav>
+                <a href="/">Integritetspolicy</a>
+                <a href="/">Villkor</a>
+                <a href="/">Support</a>
+            </nav>
+        </footer>
+    )
+}
+export default Footer
