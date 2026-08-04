ec0066d5a4a52fe161273c2e71eee4d62ba45a47
Joel Edegran <joel_edegran@hotmail.com>
2026-08-04 22:48:58 +0200

    style: change font size for h1

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -5,7 +5,7 @@ function InfoSection() {
 
   return (
     <section id="hero" className="py-20 px-12 bg-stone-50 border border-neutral-200 flex flex-col justify-center items-center gap-4">
-      <h1 className="font-bold text-4xl text-center">Lär dig bygga moderna webbapplikationer</h1>
+      <h1 className="font-bold text-[40px] text-center">Lär dig bygga moderna webbapplikationer</h1>
       <p className="text-subheading font-normal text-lg text-center">Praktiska kurser inom webbutveckling, design och moderna verktyg.</p>
       <div className="flex flex-col sm:flex-row gap-3">
         <PrimaryButton onClick={() => scrollToElement("courses")} text="Se kurser" dark={true} />
