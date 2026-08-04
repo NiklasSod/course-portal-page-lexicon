8c5ae445c562094f5ff3494a93cdaffab80c2fc6
Anton <antonroback@gmail.com>
2026-08-04 14:51:34 +0200

    Added Id to contactform for scroll on button press

--- src/components/Buttons/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton.tsx
@@ -1,12 +1,14 @@
 function PrimaryButton({
   text,
-  dark
+  dark,
+  onClick 
 }: {
   text: string;
   dark: boolean;
+  onClick?: () => void;
 }) {
   return (
-    <button className={dark ? "bg-[#2C2C2C] text-white border border-[#2C2C2C] rounded-xl px-6 py-3 mx-2" : "bg-[#FFF] text-[#2C2C2C] border border-[#2C2C2C] rounded-xl px-6 py-3 mx-2"}>
+    <button onClick={onClick} className={dark ? "bg-[#2C2C2C] text-white border border-[#2C2C2C] rounded-xl px-6 py-3 mx-2" : "bg-[#FFF] text-[#2C2C2C] border border-[#2C2C2C] rounded-xl px-6 py-3 mx-2"}>
       {text}
     </button>
   )
--- src/components/InfoSection/InfoSection.tsx
+++ src/components/InfoSection/InfoSection.tsx
@@ -2,13 +2,17 @@ import PrimaryButton from '../Buttons/PrimaryButton'
 
 function InfoSection() {
 
+  const handleScrollToContact = () => {
+    document.getElementById("contact-form")?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div className="py-20 px-12 bg-stone-50 border border-neutral-200 flex flex-col justify-center items-center gap-4">
       <h1 className="text-neutral-900 font-bold text-4xl text-center">Lär dig bygga moderna webbapplikationer</h1>
       <p className="text-neutral-600 font-normal text-lg text-center">Praktiska kurser inom webbutveckling, design och moderna verktyg.</p>
       <div className="flex flex-col sm:flex-row gap-3">
         <PrimaryButton text="Se Kurser" dark={true} />
-        <PrimaryButton text="Kontakta oss" dark={false} />
+        <PrimaryButton onClick={handleScrollToContact} text="Kontakta oss" dark={false} />
       </div>
     </div>
   )
