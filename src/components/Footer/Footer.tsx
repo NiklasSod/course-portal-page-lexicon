a83ed3a55fb2c2fc835e6cbd6f1385e69b330196
LinkTheCoder <linkhaggman@gmail.com>
2026-08-04 12:30:19 +0200

    Footer stylad med tailwind

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,12 @@
-import styles from './Footer.module.css'
-
 function Footer (){
 
     return(
-        <footer>
-            <p>Kursportalen © 2026</p>
-            <nav>
-                <a href="/">Integritetspolicy</a>
-                <a href="/">Villkor</a>
-                <a href="/">Support</a>
+        <footer className="bg-stone-50 text-stone-800 py-6 px-12 flex justify-between items-center border-t border-stone-300">
+            <p className="text-sm text-stone-500">Kursportalen © 2026</p>
+            <nav className="flex gap-8">
+                <a href="/" className="text-sm text-stone-500 hover:text-stone-800 no-underline transition-colors">Integritetspolicy</a>
+                <a href="/" className="text-sm text-stone-500 hover:text-stone-800 no-underline transition-colors">Villkor</a>
+                <a href="/" className="text-sm text-stone-500 hover:text-stone-800 no-underline transition-colors">Support</a>
             </nav>
         </footer>
     )
