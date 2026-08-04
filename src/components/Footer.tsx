1639d9c726249e8581f2f955b4ca02eb7d0f99ec
Joel Edegran <joel_edegran@hotmail.com>
2026-08-04 18:24:10 +0200

    feat(footer): use dynamic year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer (){
 
     return(
         <footer className="bg-stone-50 text-stone-800 py-6 px-6 md:px-12 flex flex-col-reverse md:flex-row justify-between items-center gap-4 md:gap-0 border-t border-stone-300">
-            <p className="text-sm text-stone-500">Kursportalen © 2026</p>
+            <p className="text-sm text-stone-500">Kursportalen &copy; {new Date().getFullYear()}</p>
             <nav className="flex gap-8">
                 <a href="/" className="text-sm text-stone-500 hover:text-stone-800 no-underline transition-colors">Integritetspolicy</a>
                 <a href="/" className="text-sm text-stone-500 hover:text-stone-800 no-underline transition-colors">Villkor</a>
