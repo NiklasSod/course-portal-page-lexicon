19247916d764a556364037189ce874ad71e21b7d
Amos Johan Persson <amosjohanpersson@gmail.com>
2026-08-05 15:31:46 +0200

    make lighter highlight on darkmode buttons

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -2,7 +2,7 @@ function PrimaryButton({
   text,
   dark,
   size = "medium",
-  onClick 
+  onClick,
 }: {
   text: string;
   dark: boolean;
@@ -11,14 +11,15 @@ function PrimaryButton({
 }) {
   const sizeStyle = size === "medium" ? "text-[15px]" : "text-[13px]";
   const variantStyles = dark
-    ? "bg-btn-dark text-white"
+    ? "bg-btn-dark text-white focus:outline-none focus:ring-2 focus:ring-teal-300"
     : "bg-[#FFF] text-btn-dark";
   return (
-    <button 
-      onClick={onClick} 
-      className={`${variantStyles} border border-btn-dark rounded-md py-[0.7em] px-[1.4em] w-max font-bold leading-none cursor-pointer ${sizeStyle}`}>
+    <button
+      onClick={onClick}
+      className={`${variantStyles} border border-btn-dark rounded-md py-[0.7em] px-[1.4em] w-max font-bold leading-none cursor-pointer ${sizeStyle}`}
+    >
       {text}
     </button>
-  )
+  );
 }
-export default PrimaryButton 
+export default PrimaryButton;
