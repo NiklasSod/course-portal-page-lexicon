8e78d695548e943d0dee741a14d5eac81246c686
Joel Edegran <joel_edegran@hotmail.com>
2026-08-06 00:02:46 +0200

    refactor(footer): rename footer navigation data variable to footerNavData

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -1,10 +1,10 @@
-import footerNavigationData from '../../public/data/footerNavigation.json';
+import footerNavData from '../../public/data/footerNavigation.json';
 
 function FooterNav() {
   return (
     <nav>
         <ul className="flex gap-8 list-none m-0 p-0">
-        {footerNavigationData.map((item) => (
+        {footerNavData.map((item) => (
             <li key={item.href}>
             <a href={item.href} className="text-sm text-footer-text hover:text-stone-500 no-underline transition-colors leading-none">
               {item.label}
